ba18ed770b6a7077078e92f853ff7c7228259813
ArahabakiGod <spacie.sharp@gmail.com>
2025-09-17 22:02:30 +0300

    Footer completed

--- src/components/footer/Links.tsx
+++ src/components/footer/Links.tsx
@@ -1,22 +1,60 @@
+import React from "react";
 import { Button, Typography } from "../common";
+import { SiTelegram, SiGithub, SiLinkedin } from "react-icons/si";
+import type { SocialLink } from "./types";
+import { useTranslation } from "react-i18next";
 
-function Links() {
+const Links: React.FC = () => {
+  const { t } = useTranslation("portfolio");
+
+  const socialLinks: SocialLink[] = [
+    {
+      name: "Telegram",
+      icon: <SiTelegram className="w-4 h-4" />,
+      href: "https://t.me/spac1e",
+      color: "text-text-primary hover:text-[#0088cc]",
+      ariaLabel: "Написать в Telegram",
+    },
+    {
+      name: "GitHub",
+      icon: <SiGithub className="w-4 h-4" />,
+      href: "https://github.com/arahabakigod",
+      color: "hover:text-text-primary",
+      ariaLabel: "Посмотреть профиль на GitHub",
+    },
+    {
+      name: "LinkedIn",
+      icon: <SiLinkedin className="w-4 h-4" />,
+      href: "https://linkedin.com/in/arahabaki",
+      color: "hover:text-[#0a66c2]",
+      ariaLabel: "Подключиться в LinkedIn",
+    },
+  ];
+
+  const handleSocialClick = (href: string) => {
+    window.open(href, '_blank', 'noopener,noreferrer');
+  };
+
   return (
-    <div className="flex flex-col">
-      <Typography variant="h4" className="pb-1">
-        My social links:
+    <div className="w-full md:w-[25%]">
+      <Typography variant="h4" color="secondary" className="mb-4">
+        {t("footer.links")}
       </Typography>
-      <Button variant="ghost" className="h-1" icon="">
-        Telegram
-      </Button>
-      <Button variant="ghost" className="h-2" icon="">
-        GitHub
-      </Button>
-      <Button variant="ghost" className="h-2" icon="">
-        LinkedIn
-      </Button>
+
+      <div className="space-y-2">
+        {socialLinks.map((social) => (
+          <Button
+            icon={social.icon}
+            variant="outlined"
+            onClick={() => handleSocialClick(social.href)}
+            className="pl-1 pb-1 pt-1 w-[75%] justify-start"
+          >
+            {social.name}
+          </Button>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default Links
+export default Links;
