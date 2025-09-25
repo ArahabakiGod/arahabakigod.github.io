796fa9b69c9ae38d843f6deb439a6c0674a05674
ArahabakiGod <spacie.sharp@gmail.com>
2025-09-26 01:49:11 +0300

    Changed some size styles

--- src/components/main/contact/ContactMe.tsx
+++ src/components/main/contact/ContactMe.tsx
@@ -21,7 +21,7 @@ const ContactMe: React.FC<ContactMeProps> = ({ className = "" }) => {
 
   return (
     <div className={className + " items-center"} id="contact-me">
-      <TextSection className="mb-4 xl:mb-0 xl:mr-8 lg:w-24 lg:min-w-24 xl:w-48 xl:min-w-48" />
+      <TextSection className="mb-4 xl:mb-0 xl:mr-8 xl:w-48 xl:min-w-48 text-center xl:text-left" />
 
       <div className="w-[100%] items-center flex flex-col justify-between border-accent pt-8 border-0 border-t-2 xl:p-0 xl:pl-12 xl:border-l-2 xl:border-t-0">
         <SocialLinks className="mb-8 w-[100%]" />
